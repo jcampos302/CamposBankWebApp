2ccb1026664fb6d7ad7596ee6c91b4a8cf7a6101
jcampos302 <59896858+jcampos302@users.noreply.github.com>
2022-04-13 09:39:09 -0500

    Added rules and Data Type for currency
    Thanks Professor for the help!

--- CamposBankWebApp/Models/Account.cs
+++ CamposBankWebApp/Models/Account.cs
@@ -5,18 +5,27 @@ namespace CamposBankWebApp.Models
     public class Account
     {
         public int ID { get; set; }
+        
         [Display(Name = "Account Name")]
+        [StringLength(12, MinimumLength = 4)]
         [Required]
         public string AccountName { get; set; }
+        
         [Display(Name = "First Name")]
+        [StringLength(18, MinimumLength = 2)]
         [Required]
         public string FirstName { get; set; }
+        
         [Display(Name = "Last Name")]
+        [StringLength(18, MinimumLength = 2)]
         [Required]
         public string LastName { get; set; }
+        
         [Required]
+        [DataType(DataType.Currency)]
         public float Balance { get; set; }
         [Required]
+        [StringLength(6, MinimumLength = 2)]
         public string Nick { get; set; }
 
     }
