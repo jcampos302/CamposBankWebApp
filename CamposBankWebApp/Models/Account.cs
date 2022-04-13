6cc4768f95cb4a74532663d08f2c0c087aa05983
jcampos302 <59896858+jcampos302@users.noreply.github.com>
2022-04-12 23:12:49 -0500

    Modified Account and Indexes

--- CamposBankWebApp/Models/Account.cs
+++ CamposBankWebApp/Models/Account.cs
@@ -1,12 +1,22 @@
-﻿namespace CamposBankWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CamposBankWebApp.Models
 {
     public class Account
     {
         public int ID { get; set; }
+        [Display(Name = "Account Name")]
+        [Required]
         public string AccountName { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public float Balance { get; set; }
+        [Required]
         public string Nick { get; set; }
 
     }
